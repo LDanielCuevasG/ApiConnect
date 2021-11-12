fcfa6861291e8e45e56739ddf64c43f1b3f37367
Luis Cuevas <ldanielcuevasg@gmail.com>
2021-11-12 17:56:50 -0600

    Library for ApiConnect

---
+++ ApiConnectLibrary/OperationResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiConnection.Objects
+{
+    public class OperationResponse<T>
+    {
+
+        public T value { get; set; }
+
+        public string detail { get; set; }
+
+        public OperationResult result { get; set; }
+
+
+        public OperationResponse() {
+            result = OperationResult.Successful;
+        }
+
+        public OperationResponse(T value)
+        {
+            this.value = value;
+            result = OperationResult.Successful;
+        }
+
+        public OperationResponse(T value, OperationResult result, string detail)
+        {
+            this.value = value;
+            this.detail = detail;
+            this.result = result;
+        }
+
+    }
+}
---
+++ ApiConnectLibrary/OperationResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiConnection.Objects
+{
+    public enum OperationResult
+    {
+        Successful,
+        Error,
+        IncompleteData,
+        Maintenance,
+        NotFound,
+        Invalid
+    }
+}
